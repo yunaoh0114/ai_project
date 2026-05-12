51a89248773aca27e2b2ad6d2bd7a54e28d8c501
yunaoh0114 <yunaoh0114@gmail.com>
2026-05-12 16:13:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ b=st.selectbox('좋아하는 음식을 선택하세요!',['빵','떡볶이','피
 if st.button('인사말 생성'):
   st.write(a+'님, 안녕하세요!')
   st.info(b+'를 좋아하시는군요!')
+  st.warning('저도 좋아해요!')
+  st.error('반가워요!')
+  st.balloons()
