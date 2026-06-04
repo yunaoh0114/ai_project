5ff6c957241b56a758b1abeb861f02f4359cf7ea
yunaoh0114 <yunaoh0114@gmail.com>
2026-06-04 13:44:47 +0900

    Update 06_수행평가.py

--- pages/06_수행평가.py
+++ pages/06_수행평가.py
@@ -25,8 +25,6 @@ st.caption("🚇 지하철역 기준 서울 맛집 추천 서비스")
 # CSV 불러오기
 # ------------------------------------------------
 from pathlib import Path
-import pandas as pd
-import streamlit as st
 
 @st.cache_data
 def load_data():
@@ -43,7 +41,6 @@ def load_data():
         st.write(f.name)
 
     return pd.DataFrame()
-   
      except:
             pass
 
